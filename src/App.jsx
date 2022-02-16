6e2d9676bf5553b027fcd997f7b7f2a64fe4254a
Dima_Sorochynskyi <dmytro.sorochynskyi@ficuslife.com>
2022-02-16 16:48:31 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Slider from "./components/slider"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Slider />
+    </div>
+  );
+}
+
+export default App;
