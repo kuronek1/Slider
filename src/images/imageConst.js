d204b37a20d63135b03ee44bcf146832d50d3655
Dima_Sorochynskyi <dmytro.sorochynskyi@ficuslife.com>
2022-02-17 15:49:27 +0200

    Add images

---
+++ src/images/imageConst.js
@@ -0,0 +1,8 @@
+import slide1 from "./img1.jpeg"
+import slide2 from "./img2.jpeg"
+import slide3 from "./img3.jpeg"
+import slide4 from "./img4.jpeg"
+import slide5 from "./img5.jpeg"
+
+const sliderList = [slide1, slide2, slide3, slide4, slide5]
+export default sliderList
