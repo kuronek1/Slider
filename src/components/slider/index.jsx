d895d4983ff933150572b022d50f0c9130865690
Dima_Sorochynskyi <dmytro.sorochynskyi@ficuslife.com>
2022-02-17 18:23:46 +0200

    Add buttons

--- src/components/slider/index.jsx
+++ src/components/slider/index.jsx
@@ -13,10 +13,30 @@ const Slides = styled.div`
   display: flex;
   height: 768px;
 `
+const Btn = styled.img`
+  position: absolute;
+  top: 50%;
+  right: 5%;
+  z-index: 10;
+  &:nth-child(even) {
+    transform: rotate(180deg);
+    left: 5%;
+  }
+`
 
 export default function Slider() {
 
 const [slide, setSlide] = useState(0);
+
+const onClickNext = () => {
+  setSlide((prevSlide) => (prevSlide + 1) % sliderList.length);
+}
+const onClickPrev = () => {
+  if(slide === 0) {
+    setSlide(sliderList.length)
+  }
+  setSlide((prevSlide) => (prevSlide - 1) % sliderList.length);
+}
 
 useEffect(() => {
   const interval = setInterval(() => {
@@ -30,6 +50,17 @@ useEffect(() => {
 
 return (
   <Div >
+    <Btn 
+      src="https://img.icons8.com/external-flatart-icons-solid-flatarticons/64/ffffff/external-arrow-arrow-flatart-icons-solid-flatarticons-6.png" 
+      id="nextBtn"
+      onClick={onClickPrev}
+      />
+    <Btn 
+      src="https://img.icons8.com/external-flatart-icons-solid-flatarticons/64/ffffff/external-arrow-arrow-flatart-icons-solid-flatarticons-6.png" 
+      id="prevBtn"
+      onClick={onClickNext}
+    />
+  
     <Slides>
       <img src={sliderList[slide]} alt={"slide № " + slide} />
     </Slides>
