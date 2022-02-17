aaf2903e0ab0cc74b7dd86f8f001bb136a73c87f
Dima_Sorochynskyi <dmytro.sorochynskyi@ficuslife.com>
2022-02-17 15:57:38 +0200

    Create slider on react

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import Slider from "./components/slider"
-import './App.css';
+import Slider from "./components/slider/index.jsx"
+import styled from "styled-components";
+
+const FullDiv = styled.div`
+  height: 98vh;
+  width: 98vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
 
 function App() {
   return (
-    <div className="App">
+    <FullDiv>
       <Slider />
-    </div>
+    </FullDiv>
   );
 }
 
--- src/components/slider/index.jsx
+++ src/components/slider/index.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import { useState, useEffect } from "react"
+import sliderList from "../../images/imageConst.js"
+import styled from "styled-components"
+
+
+const Div = styled.div`
+  position: relative;
+  width: 1360px;
+  border: 5px solid black;
+  overflow: hidden;
+`
+const Slides = styled.div`
+  display: flex;
+  height: 768px;
+`
 
 export default function Slider() {
-  return (
-    <>
-        
-    </>
-  )
+
+const [slide, setSlide] = useState(0);
+
+useEffect(() => {
+  const interval = setInterval(() => {
+    setSlide((prevSlide) => (prevSlide + 1) % sliderList.length);
+  }, 2000);
+  console.log("slide №" + slide);
+  return () => {
+    clearInterval(interval);
+  };
+}, [slide]);
+
+return (
+  <Div >
+    <Slides>
+      <img src={sliderList[slide]} alt={"slide № " + slide} />
+    </Slides>
+  </Div>
+)
 }
